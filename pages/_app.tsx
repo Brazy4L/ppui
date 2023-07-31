d42c11ef7a6e8b0267b9714d4a1bd826e0518f6d
Brazy <me@brazy.one>
2023-08-01 00:21:01 +0300

    Init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
+import '@/styles/styles.css'
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        {/* <meta name="color-scheme" content="dark" /> */}
+      </Head>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
