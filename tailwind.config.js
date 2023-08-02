7ab03701bd70f9ad004dbba7f6d747837722fb85
Brazy <me@brazy.one>
2023-08-02 17:36:49 +0300

    Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        // prefix 'dark' for dark theme, 'light' for light theme
+        // dark theme
         'dark-primary': '#955bf1', // primary color
         'dark-secondary': '#a472f3', // hover state
         'dark-bg-primary': '#121212', // main background
         'dark-bg-secondary': '#303030', // complementary background
+        'dark-bg-alternative': '#505050', // complementary background
         'dark-text-primary': '#fff', // primary text
         'dark-text-secondary': '#9b9b9b', // lower contrast text
-
+        // light theme
         'light-primary': '#8c4ffb', // primary color
         'light-secondary': '#7c37fb', // hover state
         'light-bg-primary': '#fefefe', // main background
         'light-bg-secondary': '#e1e1e1', // complementary background
+        'light-bg-alternative': '#c2c2c2', // complementary background
         'light-text-primary': '#000', // primary text
         'light-text-secondary': '#646464', // lower contrast text
       },
