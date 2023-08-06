0d916278d8ddc3045a0dfe0aa6f08a55189aa9d2
Brazy <me@brazy.one>
2023-08-06 22:35:42 +0300

    Fix css

--- components/componentsLayout.tsx
+++ components/componentsLayout.tsx
@@ -20,7 +20,7 @@ export default function ComponentsLayout({
   }, [])
 
   return (
-    <div className="justify-center gap-4 p-4 lg:grid lg:grid-cols-[350px,min(80rem,calc(100%-350px))]">
+    <div className="justify-center gap-4 p-4 lg:grid lg:grid-cols-[350px,min(80rem,calc(100%-350px-1rem))]">
       <Sidebar options={options} setOptions={setOptions} />
       <Options.Provider value={options}>
         <main className="flex w-full max-w-7xl flex-col gap-4">{children}</main>
