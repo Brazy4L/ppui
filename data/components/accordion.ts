c85e67deb7c94fdcd76407d59deb8675fcfa2693
Brazy <me@brazy.one>
2023-08-10 22:22:01 +0300

    Add svelte accordion

--- data/components/accordion.ts
+++ data/components/accordion.ts
@@ -109,7 +109,59 @@ const items = [
   </div>
 </template>
 `,
-  svelte: `svelte`,
+  svelte: `<script lang="ts">
+  import 'iconify-icon'
+
+  const items = [
+    {
+      heading: 'Item 1',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas velit in quis. Amet nostrum, porro mollitia consequatur nemo impedit odit.',
+    },
+    {
+      heading: 'Item 2',
+      content:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. A optio adipisci ipsa deserunt incidunt facere? Porro modi perferendis officia eius repellendus velit odit quae, distinctio, doloremque ipsa deleniti rem quod?',
+    },
+    {
+      heading: 'Item 3',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis facilis libero. Neque explicabo nisi iste eveniet non quidem nulla, voluptas temporibus suscipit culpa nam, laudantium, provident corrupti. Debitis doloribus ipsum dolorum fuga iusto aperiam ad perspiciatis quaerat repellendus magni!',
+    },
+    {
+      heading: 'Item 4',
+      content:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id rem dolorem quos? Delectus aperiam nihil esse. Quasi reiciendis natus totam aut eaque praesentium quaerat itaque sed, quas accusantium? Earum alias et a accusantium culpa perferendis veniam possimus minima nihil dolorum quidem maiores porro exercitationem nobis reiciendis saepe, dolore ipsa quod.',
+    },
+  ]
+</script>
+
+<div
+  class="w-full max-w-xl rounded-lg bg-light-bg-secondary dark:bg-dark-bg-secondary"
+>
+  {#each items as { heading, content }, index}
+    {#if index > 0}
+      <hr class="border-light-bg-alternative dark:border-dark-bg-alternative" />
+    {/if}
+    <details class="group">
+      <summary class="flex cursor-pointer p-4">
+        <h3 class="basis-full text-lg font-bold">{heading}</h3>
+        <iconify-icon
+          icon="material-symbols:keyboard-arrow-down-rounded"
+          class="h-fit text-light-text-secondary group-open:rotate-180 dark:text-dark-text-secondary"
+          width="28"
+          height="28"
+        />
+      </summary>
+      <p
+        class="px-4 pb-4 text-light-text-secondary dark:text-dark-text-secondary"
+      >
+        {content}
+      </p>
+    </details>
+  {/each}
+</div>
+`,
   solid: `import { For, Show } from 'solid-js'
 import { Icon } from '@iconify-icon/solid'
 
@@ -248,30 +300,79 @@ const items = [
 </script>
 
 <template>
-  <div className="flex w-full max-w-xl flex-col gap-4">
+  <div class="flex w-full max-w-xl flex-col gap-4">
     <details
-      className="group rounded-lg bg-light-bg-secondary dark:bg-dark-bg-secondary"
+      class="group rounded-lg bg-light-bg-secondary dark:bg-dark-bg-secondary"
       v-for="item in items"
     >
-      <summary className="flex cursor-pointer p-4">
-        <span className="basis-full text-lg font-bold">{{ item.heading }}</span>
+      <summary class="flex cursor-pointer p-4">
+        <span class="basis-full text-lg font-bold">{{ item.heading }}</span>
         <iconify-icon
           icon="material-symbols:keyboard-arrow-down-rounded"
-          className="h-fit text-light-text-secondary group-open:rotate-180 dark:text-dark-text-secondary"
+          class="h-fit text-light-text-secondary group-open:rotate-180 dark:text-dark-text-secondary"
           width="28"
           height="28"
         />
       </summary>
       <p
-        className="px-4 pb-4 text-light-text-secondary dark:text-dark-text-secondary"
+        class="px-4 pb-4 text-light-text-secondary dark:text-dark-text-secondary"
       >
         {{ item.content }}
       </p>
     </details>
   </div>
-</template>
+</template>  
 `,
-  svelte: `svelte`,
+  svelte: `<script lang="ts">
+  import 'iconify-icon'
+
+  const items = [
+    {
+      heading: 'Item 1',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas velit in quis. Amet nostrum, porro mollitia consequatur nemo impedit odit.',
+    },
+    {
+      heading: 'Item 2',
+      content:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. A optio adipisci ipsa deserunt incidunt facere? Porro modi perferendis officia eius repellendus velit odit quae, distinctio, doloremque ipsa deleniti rem quod?',
+    },
+    {
+      heading: 'Item 3',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis facilis libero. Neque explicabo nisi iste eveniet non quidem nulla, voluptas temporibus suscipit culpa nam, laudantium, provident corrupti. Debitis doloribus ipsum dolorum fuga iusto aperiam ad perspiciatis quaerat repellendus magni!',
+    },
+    {
+      heading: 'Item 4',
+      content:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id rem dolorem quos? Delectus aperiam nihil esse. Quasi reiciendis natus totam aut eaque praesentium quaerat itaque sed, quas accusantium? Earum alias et a accusantium culpa perferendis veniam possimus minima nihil dolorum quidem maiores porro exercitationem nobis reiciendis saepe, dolore ipsa quod.',
+    },
+  ]
+</script>
+
+<div class="flex w-full max-w-xl flex-col gap-4">
+  {#each items as { heading, content }}
+    <details
+      class="group rounded-lg bg-light-bg-secondary dark:bg-dark-bg-secondary"
+    >
+      <summary class="flex cursor-pointer p-4">
+        <span class="basis-full text-lg font-bold">{heading}</span>
+        <iconify-icon
+          icon="material-symbols:keyboard-arrow-down-rounded"
+          class="h-fit text-light-text-secondary group-open:rotate-180 dark:text-dark-text-secondary"
+          width="28"
+          height="28"
+        />
+      </summary>
+      <p
+        class="px-4 pb-4 text-light-text-secondary dark:text-dark-text-secondary"
+      >
+        {content}
+      </p>
+    </details>
+  {/each}
+</div>
+`,
   solid: `import { For } from 'solid-js'
 import { Icon } from '@iconify-icon/solid'
 
