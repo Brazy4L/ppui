56232afd8f93b7497c6943c35626e60fb14ecc12
Brazy <me@brazy.one>
2023-08-11 15:21:34 +0300

    Update tw

--- pages/components/index.tsx
+++ pages/components/index.tsx
@@ -4,27 +4,27 @@ import Link from 'next/link'
 
 const codeBlocks = {
   tw: `module.exports = {
-content: [], // paths to your files
-// darkMode: 'class', // uncomment to toggle theme manually
-theme: {
-  extend: {
-    colors: {
-      // dark theme
-      'dark-primary': '#b687fb', // primary color
-      'dark-secondary': '#d5bafd', // close to ↑
-      'dark-bg-primary': '#121212', // main background
-      'dark-bg-secondary': '#1e1e1e', // complementary background
-      'dark-bg-alternative': '#2b2b2b', // complementary background
-      'dark-text-primary': '#fff', // primary text
-      'dark-text-secondary': '#858585', // lower contrast text
-      // light theme
-      'light-primary': '#7402ed', // primary color
-      'light-secondary': '#5102a7', // close to ↑
-      'light-bg-primary': '#fefefe', // main background
-      'light-bg-secondary': '#f9f7fd', // complementary background
-      'light-bg-alternative': '#ede7f9', // complementary background
-      'light-text-primary': '#000', // primary text
-      'light-text-secondary': '#727272', // lower contrast text
+  content: [], // paths to your files
+  // darkMode: 'class', // uncomment to toggle theme manually
+  theme: {
+    extend: {
+      colors: {
+        // dark theme
+        'dark-primary': '#b687fb', // primary color
+        'dark-secondary': '#d5bafd', // close to ↑
+        'dark-bg-primary': '#121212', // main background
+        'dark-bg-secondary': '#1e1e1e', // complementary background
+        'dark-bg-alternative': '#2b2b2b', // complementary background
+        'dark-text-primary': '#fff', // primary text
+        'dark-text-secondary': '#858585', // lower contrast text
+        // light theme
+        'light-primary': '#7402ed', // primary color
+        'light-secondary': '#5102a7', // close to ↑
+        'light-bg-primary': '#fefefe', // main background
+        'light-bg-secondary': '#f9f7fd', // complementary background
+        'light-bg-alternative': '#ede7f9', // complementary background
+        'light-text-primary': '#000', // primary text
+        'light-text-secondary': '#727272', // lower contrast text
       },
     },
   },
