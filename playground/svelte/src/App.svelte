0bdbce9b8a6d471231f1fb57edf298bba23f855c
Brazy <me@brazy.one>
2023-08-17 00:24:17 +0300

    Add svelte

---
+++ playground/svelte/src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import AccordionRegularOne from './components/accordion/AccordionRegularOne.svelte'
+  import AccordionRegularTwo from './components/accordion/AccordionRegularTwo.svelte'
+</script>
+
+<div>
+  <AccordionRegularOne />
+  <AccordionRegularTwo />
+</div>
---
+++ playground/svelte/src/components/accordion/AccordionRegularTwo.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import MaterialSymbolsKeyboardArrowDownRounded from '~icons/material-symbols/keyboard-arrow-down-rounded'
+
+  const items = [
+    {
+      heading: 'Item 1',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas velit in quis. Amet nostrum, porro mollitia consequatur nemo impedit odit.',
+    },
+    {
+      heading: 'Item 2',
+      content:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. A optio adipisci ipsa deserunt incidunt facere? Porro modi perferendis officia eius repellendus velit odit quae, distinctio, doloremque ipsa deleniti rem quod?',
+    },
+    {
+      heading: 'Item 3',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis facilis libero. Neque explicabo nisi iste eveniet non quidem nulla, voluptas temporibus suscipit culpa nam, laudantium, provident corrupti. Debitis doloribus ipsum dolorum fuga iusto aperiam ad perspiciatis quaerat repellendus magni!',
+    },
+    {
+      heading: 'Item 4',
+      content:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id rem dolorem quos? Delectus aperiam nihil esse. Quasi reiciendis natus totam aut eaque praesentium quaerat itaque sed, quas accusantium? Earum alias et a accusantium culpa perferendis veniam possimus minima nihil dolorum quidem maiores porro exercitationem nobis reiciendis saepe, dolore ipsa quod.',
+    },
+  ]
+</script>
+
+<div class="flex w-full max-w-xl flex-col gap-4">
+  {#each items as { heading, content }}
+    <details
+      class="group rounded-lg bg-light-bg-secondary dark:bg-dark-bg-secondary"
+    >
+      <summary class="flex cursor-pointer p-4">
+        <span class="basis-full text-lg font-bold">{heading}</span>
+        <MaterialSymbolsKeyboardArrowDownRounded
+          class="text-light-text-secondary group-open:rotate-180 dark:text-dark-text-secondary"
+          width="28"
+          height="28"
+        />
+      </summary>
+      <p
+        class="px-4 pb-4 text-light-text-secondary dark:text-dark-text-secondary"
+      >
+        {content}
+      </p>
+    </details>
+  {/each}
+</div>
