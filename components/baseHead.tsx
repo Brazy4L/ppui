4705c0e318adcac6dca6bbb00a7380208be6f2d3
Brazy <me@brazy.one>
2023-08-17 00:44:37 +0300

    Fix types

--- components/baseHead.tsx
+++ components/baseHead.tsx
@@ -1,6 +1,12 @@
 import Head from 'next/head'
 
-export default function BaseHead({ title, description }: { title: string, description: string }) {
+export default function BaseHead({
+  title,
+  description,
+}: {
+  title: string
+  description: string
+}) {
   return (
     <Head>
       <title>{`${title} | PPUI`}</title>
