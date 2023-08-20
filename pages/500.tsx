b3b544133a56182a2da6922d13ff7169474e4e29
Brazy <me@brazy.one>
2023-08-20 13:32:41 +0300

    Add 404 and 500 pages

---
+++ pages/500.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import BaseHead from '@/components/baseHead'
+
+export default function Custom500() {
+  return (
+    <main className="mx-auto flex min-h-[calc(100vh-64px)] max-w-xl flex-col justify-center gap-4 p-4">
+      <BaseHead title="500" description="Secret 500 Page" />
+      <h1 className="text-center font-mono text-xl">
+        Secret <span className="font-semibold">500</span> Page
+      </h1>
+      <hr className="border-light-bg-alternative dark:border-dark-bg-alternative" />
+      <Link
+        className="rounded-lg bg-light-primary p-3 text-center font-semibold text-light-bg-primary transition-colors hover:bg-light-secondary dark:bg-dark-primary dark:text-dark-bg-primary dark:hover:bg-dark-secondary"
+        href="/"
+      >
+        Home
+      </Link>
+    </main>
+  )
+}
