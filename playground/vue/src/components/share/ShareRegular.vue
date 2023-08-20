63e0811b59fffb6d283fee9eb998554682833209
Brazy <me@brazy.one>
2023-08-20 17:49:11 +0300

    Add title to share component

--- playground/vue/src/components/share/ShareRegular.vue
+++ playground/vue/src/components/share/ShareRegular.vue
@@ -16,6 +16,7 @@ const shareLink = () => {
   <button
     class="rounded-full bg-light-bg-secondary p-2 transition-colors hover:bg-light-bg-alternative dark:bg-dark-bg-secondary dark:hover:bg-dark-bg-alternative"
     v-on:click="shareLink"
+    title="Share"
   >
     <MaterialSymbolsShare
       className="text-light-text-secondary dark:text-dark-text-secondary"
