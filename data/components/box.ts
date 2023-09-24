b7616dea191242b67c4d28c9c8170eeecbda41ca
Brazy <me@brazy.one>
2023-09-24 16:39:30 +0300

    Add Truncated Box

---
+++ data/components/box.ts
@@ -0,0 +1,26 @@
+export const boxRegular = {
+  react: [
+    {
+      tab: 'Box.tsx',
+      path: 'components/components/box/BoxRegular.tsx',
+    },
+  ],
+  vue: [
+    {
+      tab: 'Box.vue',
+      path: 'playground/vue/src/components/box/BoxRegular.vue',
+    },
+  ],
+  svelte: [
+    {
+      tab: 'Box.svelte',
+      path: 'playground/svelte/src/components/box/BoxRegular.svelte',
+    },
+  ],
+  solid: [
+    {
+      tab: 'Box.tsx',
+      path: 'playground/solid/src/components/box/BoxRegular.tsx',
+    },
+  ],
+}
---
+++ playground/svelte/src/components/box/BoxRegular.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const text =
+    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, repellendus modi quod quasi sunt voluptatem provident ex? Quis, officia provident sed ut iste quod delectus dolorem recusandae sit nihil? Perspiciatis temporibus ducimus minus reiciendis consectetur, id, vero, blanditiis mollitia ea deleniti aliquid. Vel harum odit corrupti beatae veniam quos ratione, sequi veritatis adipisci quis minima temporibus libero dolor quod perspiciatis at reiciendis nisi. Voluptas sunt repellat quidem omnis? Explicabo tempora totam, nisi enim veritatis ipsum reiciendis libero sit consequatur obcaecati dolorem nulla illum velit neque quisquam expedita quibusdam! Blanditiis cumque esse impedit dignissimos nesciunt quae commodi magnam accusamus qui repellendus!'
+
+  let more = false
+</script>
+
+<div class="max-w-xl">
+  <p class={more ? '' : 'line-clamp-3 '}>{text}</p>
+  <button
+    class="mt-4 flex w-full justify-center rounded-lg bg-light-bg-secondary p-3 font-semibold transition-colors hover:bg-light-bg-alternative dark:bg-dark-bg-secondary dark:hover:bg-dark-bg-alternative"
+    on:click={() => (more = !more)}
+  >
+    {more ? 'less' : 'MORE'}
+  </button>
+</div>
